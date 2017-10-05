8620e014e886603806ce99e4ff82975b80b587a1
Victor Coatalem <victor.coatalem@epita.fr>
2017-10-05 19:57:50 +0200

    first try

---
+++ SDL/pixel_operations.c
@@ -0,0 +1,176 @@
+// pixel_operations.c
+ 
+// Simple get/put pixel for SDL
+// Inspired by code from SDL documentation
+// (http://www.libsdl.org/release/SDL-1.2.15/docs/html/guidevideo.html)
+ 
+# include "pixel_operations.h"
+ 
+static inline
+Uint8* pixelref(SDL_Surface *surf, unsigned x, unsigned y) {
+  int bpp = surf->format->BytesPerPixel;
+  return (Uint8*)surf->pixels + y * surf->pitch + x * bpp;
+}
+ 
+Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y) {
+  Uint8 *p = pixelref(surface, x, y);
+  switch(surface->format->BytesPerPixel) {
+  case 1:
+    return *p;
+  case 2:
+    return *(Uint16 *)p;
+  case 3:
+    if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+      return p[0] << 16 | p[1] << 8 | p[2];
+    else
+      return p[0] | p[1] << 8 | p[2] << 16;
+  case 4:
+    return *(Uint32 *)p;
+  }
+  return 0;
+}
+ 
+void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel) {
+  Uint8 *p = pixelref(surface, x, y);
+  switch(surface->format->BytesPerPixel) {
+  case 1:
+    *p = pixel;
+    break;
+  case 2:
+    *(Uint16 *)p = pixel;
+    break;
+  case 3:
+    if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+      p[0] = (pixel >> 16) & 0xff;
+      p[1] = (pixel >> 8) & 0xff;
+      p[2] = pixel & 0xff;
+    } else {
+      p[0] = pixel & 0xff;
+      p[1] = (pixel >> 8) & 0xff;
+      p[2] = (pixel >> 16) & 0xff;
+    }
+    break;
+  case 4:
+    *(Uint32 *)p = pixel;
+    break;
+  }
+}
+
+
+Uint32 greyconvert(Uint32 pixel, SDL_Surface* screen)
+{
+  Uint8 r = 0;
+  Uint8 g = 0;
+  Uint8 b = 0;
+
+  SDL_GetRGB(pixel, screen->format, &r, &g, &b);
+  Uint8  avg = r * 0.3 + g * 0.59 + b * 0.11;
+  return SDL_MapRGB(screen->format, avg, avg, avg);
+}
+
+
+void forall_greyconvert(SDL_Surface* screen)
+{
+  unsigned w = 0;
+  unsigned h = 0;
+  w = screen->w;
+  h = screen->h;
+  for(unsigned i = 0; i < w; i++)
+  {
+	for(unsigned j = 0; j < h; j++)
+	{
+		Uint32 pixel = greyconvert(getpixel(screen, i, j), screen);
+		putpixel(screen, i, j, pixel);
+	}
+  }
+}
+
+
+
+
+void sobel_filter(SDL_Surface* screen, SDL_Surface* screen2)  //horizontal filter
+{	
+  int weight[3][3] ={   {	-1,	0,	1},
+	      		{	-2,	0,	2},
+			{	-1,	0,	1}};
+
+ 
+  const SDL_PixelFormat* pixel_format = screen->format;
+  Uint8 r;
+  Uint8 g;
+  Uint8 b;
+
+  Uint8 pixel_red_value = 0;
+  Uint32 current_pixel_value = 0;  
+
+//calculating min and max  
+
+  double max = 255;
+  double min = 0;
+
+  unsigned w = screen->w;
+  unsigned h = screen->h;
+
+  for(unsigned i = 0; i < w; i++)
+  {
+	for(unsigned j = 0; j < h; j++)
+	{
+		pixel_red_value = 0;
+		for (unsigned x = -1; x < 2; x++)
+		{
+			for(unsigned y = -1; y < 2; y++)
+			{
+				current_pixel_value = getpixel(screen, i, j);
+				SDL_GetRGB(current_pixel_value, pixel_format, &r, &g, &b);
+				pixel_red_value += weight[x + 1][y + 1] * r/*current_pixel_value*/;
+			}
+		}
+		if (pixel_red_value < min)
+		{
+			min = pixel_red_value;
+		}
+		if (pixel_red_value > max)
+		{
+			max = pixel_red_value;
+		}
+	}
+  }
+
+ //regenerating screen2
+
+  w = screen2->w;
+  h = screen2->h;
+
+  for(unsigned i = 0; i < w; i++)
+  {
+	for(unsigned j = 0; j < h; j++)
+	{
+		pixel_red_value = 0;
+		for (unsigned x = -1; x < 2; x++)
+		{
+			for(unsigned y = -1; y < 2; y++)
+			{
+				current_pixel_value = getpixel(screen, i, j);
+				SDL_GetRGB(current_pixel_value, pixel_format, &r, &g, &b);
+				pixel_red_value += weight[x + 1][y + 1] * r/*current_pixel_value*/;
+			}
+		}
+		pixel_red_value = 255 * (pixel_red_value - min) / (max - min);
+		Uint32 pixel = SDL_MapRGB(pixel_format, pixel_red_value, pixel_red_value, pixel_red_value);
+		putpixel(screen2, i, j, pixel);
+
+	}
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ SDL/pixel_operations.h
@@ -0,0 +1,15 @@
+// pixel_operations.h
+ 
+# ifndef PIXEL_OPERATIONS_H_
+# define PIXEL_OPERATIONS_H_
+ 
+# include <stdlib.h>
+# include <SDL.h>
+ 
+Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y);
+void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
+Uint32 greyconvert(Uint32 pixel, SDL_Surface* screen);
+void forall_greyconvert(SDL_Surface* screen);
+void sobel_filter(SDL_Surface* screen, SDL_Surface* screen2);
+ 
+# endif
