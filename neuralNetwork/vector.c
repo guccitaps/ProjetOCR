979bfceb06b2984bd24c492ece28576f9491fdc0
Victor Coatalem <victor.coatalem@epita.fr>
2017-10-11 18:04:07 +0200

    up

--- neuralNetwork/vector.c
+++ neuralNetwork/vector.c
@@ -9,6 +9,11 @@ unsigned long y;
 struct Vector {
 struct Coord start;
 struct Coord end;
+
+unsigned long length; //sqrt((end.x - start.c)^2 + (end.y - start.y)^2)
+unsigned long direction; // 0  pi/4  pi/2  3pi/4
+unsigned long density; //amount of other Vectors assimilated
+
 } ;
 
 void Coord_Init(struct Coord coo, unsigned long x, unsigned long y)
@@ -21,6 +26,12 @@ void Vector_Init(struct Vector v, unsigned long x, unsigned long y, unsigned lon
 {
 	Coord_Init(v.start, x, y);
 	Coord_Init(v.end, x2, y2);
+}
+
+bool Mergeable(struct Vector u, struct Vector v)
+{
+
+	
 }
 
 int main(int argc, char* argv[])
