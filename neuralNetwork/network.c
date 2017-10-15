aa132ebe4cf3c715835a409ae5de5063640ee221
Victor Coatalem <victor.coatalem@epita.fr>
2017-10-15 10:10:24 +0200

    debut neurones

---
+++ neuralNetwork/network.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "network.h"
+_Bool same(_Bool a, _Bool b)
+ { 
+	return a == b;
+ }
+
+_Bool activation(int[] neurons, int[] weights, int size)
+ {	
+	int sum = 0;
+	for(unsigned i = 0; i < size; i++)
+	{
+	//	sum += neu
+	}	
+ }
+
+int main(int argc, char* argv[])
+{
+	int neurons[1]; // 1 -> activated ; 0 -> not
+	int weights[1]; // xor -> 1 seul neurone : A != B
+
+	//get question
+
+	//analyse
+
+	neurons[0] = same();
+	//find answer
+
+	//correction
+	return 0;
+}
+
+
+
---
+++ neuralNetwork/network.h
@@ -0,0 +1,16 @@
+
+/*
+ * network.h
+ *
+ * 
+ */
+ 
+#ifndef _NEURALNETWORK_NETWORK_H_
+#define _NEURALNETWORK_NETWORK_H_
+ 
+/*
+ * func_t: function pointer type
+ */
+typedef _Bool (*func_t)(_Bool);
+ 
+#endif /* _NEURALNETWORK_NETWORK_H__ */
